84a8e9ca71e6b67bd756f9e54260d8f1663e7c53
Ravi Giri <kajalbyaspandey@Ravis-MacBook-Air.local>
2026-02-19 09:52:12 +0530

    checkout logic added

--- backend/store/models.py
+++ backend/store/models.py
@@ -29,7 +29,7 @@ class UserProfile(models.Model):
         return self.user.username
 
 class Order(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     total_amount = models.DecimalField(max_digits=10, decimal_places=2)
 
@@ -38,7 +38,7 @@ class Order(models.Model):
 
 class OrderItem(models.Model):
     order = models.ForeignKey(Order, related_name="items", on_delete=models.CASCADE)
-    Product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
     quantity = models.PositiveIntegerField(default=1)
     price = models.DecimalField(max_digits=10, decimal_places=2)
 
